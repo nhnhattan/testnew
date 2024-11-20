4dea3be4d18027d98e721f73559a4bb2d037f1bf
nhnhattan <nhoxcon029@gmail.com>
2024-11-20 17:02:37 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+[
+  { AiId: 6, UserAiFinal: "", CreateDate: "2024-09-25T18:35:49.477" },
+  { AiId: 5, UserAiFinal: "", CreateDate: "2024-09-25T18:28:04.297" },
+  { AiId: 4, UserAiFinal: "", CreateDate: "2024-09-20T05:56:57.830" },
+  { AiId: 3, UserAiFinal: "", CreateDate: "2024-09-20T05:46:51.733" },
+  { AiId: 2, UserAiFinal: final.png, CreateDate: "2024-09-20T05:35:37.297" },
+];
+    
